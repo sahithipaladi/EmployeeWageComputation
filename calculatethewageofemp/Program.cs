54088d8aa003f64d1a1774de4ba782ef18e64fde
sahithi <sahithipari99@gmail.com>
2021-06-28 16:24:02 +0530

    employee wage2

---
+++ calculatethewageofemp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace calculatethewageofemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeePresent = 1;
+            int empHours = 0;
+            int empRatePerHour = 20;
+            int empWage = 0;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            if (empInput == employeePresent)
+            {
+                Console.WriteLine("Employee is Present");
+                empHours = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHours = 0;
+            }
+            empWage = empRatePerHour * empHours;
+            Console.WriteLine("Employee wage per day =" + empWage);
+            Console.Read();
+        }
+    }
+}
