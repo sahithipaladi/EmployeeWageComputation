8894dd47b4b360a5067f25e293ccfcbdea24eb62
sahithi <sahithipari99@gmail.com>
2021-06-28 15:46:21 +0530

    employee wage1

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeepresent = 1;
+            int empHours = 8;
+            int empRatePerHour = 20;
+
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            if (empInput==employeepresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.Read();
+
+
+        }
+    }
+}
