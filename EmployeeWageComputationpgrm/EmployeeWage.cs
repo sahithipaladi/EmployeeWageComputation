1a75a268fc94267906af6aa7abcde35e63314038
sahithipaladi <86294777+sahithipaladi@users.noreply.github.com>
2021-07-04 21:17:44 +0530

    Update EmployeeWage.cs
    small  changes

--- EmployeeWageComputationpgrm/EmployeeWage.cs
+++ EmployeeWageComputationpgrm/EmployeeWage.cs
@@ -6,5 +6,17 @@ namespace EmployeeWageComputationpgrm
 {
     class EmployeeWage
     {
+         public String companyName;
+        public int empWagePerHour;
+        public int maxWorkingDays;
+        public int maxWorkingHours;
+        public int totalEmpWage;
+
+        public EmployeeWage(String companyName, int empWagePerHour, int maxWorkingDays, int maxWorkingHours)
+        {
+            this.companyName = companyName;
+            this.empWagePerHour = empWagePerHour;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHours = maxWorkingHours; 
     }
 }
