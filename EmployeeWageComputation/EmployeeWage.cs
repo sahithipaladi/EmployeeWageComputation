04a5ff0fbc7b9a27edd39f8a714ec3649d1a64ed
sahithi <sahithipari99@gmail.com>
2021-07-04 13:29:39 +0530

    [sahithi] addingwageformultiplecompanines

---
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageCompution
+{
+    class EmployeeWage
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        // public const int WAGE_PER_HR = 20;
+        //public const int MAX_WORKING_DAYS = 20;
+        //public const int MAX_WORKING_HRS = 100;
+        public static void Compute(string CompanyName, int WagePerHr, int MaxWorkingDays, int MaxWorkingHrs)
+        {
+            Random random = new Random();
+            int empHrs = 0;
+            int empWage = 0;
+            int totalWage = 0;
+            int day = 1;
+            int empWorkingHrs = 0;
+            while (day <= MaxWorkingDays && empWorkingHrs < MaxWorkingHrs)
+            {
+                int empInput = random.Next(0, 3);
+                switch (empInput)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                empWage = empHrs * WagePerHr;
+                empWorkingHrs += empHrs;
+                totalWage += empWage;
+                if (empInput != 0)
+                    day++;
+            }
+            Console.WriteLine("Wage for " + CompanyName + " employees for " + day + " working days per month : " + totalWage);
+        }
+    }
+}
---
+++ EmployeeWageComputation/program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace EmployeeWageCompution
+{ 
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Employee Wage Computation");
+        EmployeeWage.Compute("Dmart", 20, 20, 100);
+        EmployeeWage.Compute("Reliance", 50, 20, 75);
+    }
+}
+}
+
+    
