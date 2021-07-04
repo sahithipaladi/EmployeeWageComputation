603273a600f5c10a69a911da69fd525e2d8c4fbc
sahithipaladi <86294777+sahithipaladi@users.noreply.github.com>
2021-07-04 21:26:36 +0530

    Update Program.cs

--- EmployeeWageComputationpgrm/Program.cs
+++ EmployeeWageComputationpgrm/Program.cs
@@ -6,7 +6,12 @@ namespace EmployeeWageComputationpgrm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("\n Employee Wage Computation \n");
+         
+            CompanyEmpWage companyEmpWage = new CompanyEmpWage();
+            companyEmpWage.addCompanyEmpWage("Dmart", 20, 20, 100);
+            companyEmpWage.addCompanyEmpWage("Reliance", 20, 20, 100);
+            companyEmpWage.computeEmpWage(); 
         }
     }
 }
